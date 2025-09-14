8d623e2b8e6a837d19259ebf18e13b5b5ea9b2ce
sharath <2004sharath@gmail.com>
2025-09-14 13:02:22 +0530

    commit

--- React-project/Experiment-0/vite.config.js
+++ React-project/Experiment-0/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+// Update vite.config.js File on Local
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    allowedHosts: true
+  }
 })
